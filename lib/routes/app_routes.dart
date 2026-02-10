4a39e9fa85ae268f32be18434373a2a5b784fe17
Rimon Chakma <rimon.chakma880@gmail.com>
2026-02-10 23:27:52 +0600

    add appRoutes

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,16 @@
+class AppRoutes {
+  static const String splash = "/";
+  static const String home = "/home";
+  static const String main = "/main";
+  static const String login = "/login";
+  static const String register = "/register";
+  static const String forgetPassword = "/forget_password";
+  static const String changePassword = "/change_password";
+  static const String profile = "/profile";
+  static const String chat = "/chat";
+  static const String userList = "/user_list";
+  static const String friends = "/friends";
+  static const String friendRequest = "/friend_requests";
+  static const String notification = "/notification";
+
+}
